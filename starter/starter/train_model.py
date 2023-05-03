c14f058963755915a46a9906aa3719cc973ad3bf
Marc Fabian Mezger <57255687+mfmezger@users.noreply.github.com>
2023-05-03 17:07:58 +0000

    refactoring.

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,51 +1,69 @@
-# Script to train machine learning model.
+"""Script to train machine learning model."""
 import os
+import pickle
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from starter.ml.data import process_data
 from starter.ml.model import train_model, inference, compute_model_metrics
-import pickle
-# Add the necessary imports for the starter code.
-import pandas as pd
 
-# Add code to load in the data.
-data = pd.read_csv("data/census.csv")
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
-file_path = "/starter/model/"
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
 
-# Proces the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-)
+def main():
+    # Add code to load in the data.
+    data = pd.read_csv("data/census.csv")
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
+    FILE_PATH = "/starter/model/"
 
-# Train and save a model.
-ada_boost = train_model(X_train, y_train)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-model_path = os.path.join(file_path, '/model/rf_model.pkl')
-pickle.dump(ada_boost, open(model_path, 'wb'))
+    # Proces the test data with the process_data function.
+    X_test, y_test, encoder, lb = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb,
+    )
 
-encoder_path = os.path.join(file_path, '/model/encoder.pkl')
-pickle.dump(encoder, open(encoder_path, 'wb'))
+    # Train and save a model.
+    ada_boost = train_model(X_train, y_train)
 
-lb_path = os.path.join(file_path, '/model/lb.pkl')
-pickle.dump(lb, open(lb_path, 'wb'))
+    model_path = os.path.join(FILE_PATH, "ada_boost.pkl")
+    encoder_path = os.path.join(FILE_PATH, "encoder.pkl")
+    lb_path = os.path.join(FILE_PATH, "lb.pkl")
 
-# Predict
-preds = inference(rf_model, X_test)
+    # Save AdaBoost model
+    with open(model_path, "wb") as model_file:
+        pickle.dump(ada_boost, model_file)
 
-print('precision: {}, recall: {}, fbeta: {}'.format(
-    *compute_model_metrics(y_test, preds)
-))
+    # Save OneHotEncoder
+    with open(encoder_path, "wb") as encoder_file:
+        pickle.dump(encoder, encoder_file)
+
+    # Save LabelBinarizer
+    with open(lb_path, "wb") as lb_file:
+        pickle.dump(lb, lb_file)
+
+    # Predict
+    preds = inference(ada_boost, X_test)
+
+    print(
+        "precision: {}, recall: {}, fbeta: {}".format(*compute_model_metrics(y_test, preds))
+    )
+
+
+if __name__ == "__main__":
+    main()
